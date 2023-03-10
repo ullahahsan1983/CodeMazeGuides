bfcdd4cef847dbeb800a6b85f68b7c05c8ca36f9
Ahsan Ullah <ullahahsan1983@gmail.com>
2023-03-10 01:32:47 -0500

    Added code sample for Static file config

---
+++ aspnetcore-features/ReadAppSettings/ReadAppSettings/Controllers/StaticFileDemoController.cs
@@ -0,0 +1,22 @@
+﻿namespace ReadAppSettings.Controllers;
+
+[Route("static")]
+[ApiController]
+public class StaticFileDemoController : ControllerBase
+{
+    private readonly FormatSettings _formatSettings;
+
+    public StaticFileDemoController(IWebHostEnvironment environment)
+    {
+        var configFile = environment.WebRootFileProvider.GetFileInfo("static_config.json");
+
+        var config = new ConfigurationBuilder()
+            .AddJsonFile(configFile.PhysicalPath)
+            .Build();
+
+        _formatSettings = config.GetSection("Formatting").Get<FormatSettings>();
+    }
+
+    [HttpGet("formatting")]
+    public FormatSettings Get() => _formatSettings;
+}
